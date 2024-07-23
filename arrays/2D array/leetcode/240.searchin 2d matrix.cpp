48a3dc0391237dea841ca7a95c3ed88fad01d111
Shivam Tripathi <tripathishubham095@gmail.com>
2024-07-23 10:58:23 +0530

    2D arrays update

--- arrays/2D array/leetcode/240.searchin 2d matrix.cpp
+++ arrays/2D array/leetcode/240.searchin 2d matrix.cpp
@@ -1,1 +1,24 @@
-9p['']
+// Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+
+// Integers in each row are sorted in ascending from left to right.
+// Integers in each column are sorted in ascending from top to bottom.
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+        int rowindex = 0;
+        int colindex = col -1 ;
+        while(rowindex < row && colindex >=0){
+            int element = matrix[rowindex][colindex];
+            if(element == target){
+                return 1;
+            }else if(element > target){
+                colindex --;
+            }else{
+                rowindex++;
+            }
+        }
+        return 0;
+    }
+};
